464f40651fb70760dbc28a8c700b054336460a7d
wwsmith <warner.smith@symphona.us>
2024-07-24 17:12:38 -0400

    Fixing error in final write

--- EmailCompromiseInvestigation/parse_out_MIDs.py
+++ EmailCompromiseInvestigation/parse_out_MIDs.py
@@ -4,22 +4,22 @@ org = input("OrgName: ")
 user = input("userID: ")
 
 text = '''
-#insert string here. This can be any string containing MessageIds in the standard <message.id.format> 
+#insert string here. This can be any string containing MessageIds in the standard <message.id.format>
 '''
 
 #find every string matching the regex pattern for a standard MessageId and add it to "matches"
 matches = re.findall(r'(<[^>]+>)', text)
 #if "matches" is not empty
 if len(matches) != 0:
-    print(f'Found {len(matches)} MessageIds.')
     #dedupe
-    newMatches = set(matches)
+    dedupedMatches = set(matches)
+    print(f'Found {len(dedupedMatches)} unique MessageIds.')
     #open file to write formatted MessageIds
     with open(f'./inFile_{org}_{user}.txt',"w+") as f:
         print(f"Writing MessageIds to 'inFile_{org}_{user}.txt' in the current directory...")
         #writing to file
-        for match in matches:
-            f.write(f"{match}\n")
+        for i in dedupedMatches:
+            f.write(f"{i}\n")
 #otherwise
 else:
     print("Error: no MessageIds found in provided text.")
