74405c65db83bc8500cb5fd56ada1f7a5f49d6bf
Warner Smith <109105167+wsm1th@users.noreply.github.com>
2024-07-24 16:43:19 -0400

    Create parse_out_MIDs.py
    Python script that parses MessageIDs from text

---
+++ parse_out_MIDs.py
@@ -0,0 +1,25 @@
+import re
+
+org = input("OrgName: ")
+user = input("userID: ")
+
+text = '''
+#insert string here. This can be any string containing MessageIds in the standard <message.id.format> 
+'''
+
+#find every string matching the regex pattern for a standard MessageId and add it to "matches"
+matches = re.findall(r'(<[^>]+>)', text)
+print(f'Found {len(matches)} MessageIds.')
+#if "matches" is not empty
+if len(matches) != 0:
+    #dedupe
+    newMatches = set(matches)
+    #open file to write formatted MessageIds
+    with open(f'./inFile_{org}_{user}.txt',"w+") as f:
+        print(f"Writing MessageIds to 'inFile_{org}_{user}.txt' in the current directory...")
+        #writing to file
+        for match in matches:
+            f.write(f"{match}\n")
+#otherwise
+else:
+    print("Error: no MessageIds found in provided text.")
